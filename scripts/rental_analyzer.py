b5b1fcb5b7bf55b17b95e4f973273037ca5608f8
j6tan <doritanjiayi@gmail.com>
2026-02-21 21:36:03 -0800

    Update rental_analyzer.py

--- scripts/rental_analyzer.py
+++ scripts/rental_analyzer.py
@@ -350,3 +350,11 @@ if full_analysis_list:
         with m4: styled_metric("Annual Mortgage", top['Annual Mortg'], DARK_RED)
 
 show_disclaimer()
+
+# --- FOOTER ---
+st.markdown("""
+    <div style="text-align: center; color: #adb5bd; font-size: 0.85em; margin-top: 50px; padding-top: 20px; border-top: 1px solid #dee2e6;">
+        &copy; 2026 FIRE Calculator. All rights reserved. <br>
+        <span style="font-size: 0.9em; font-style: italic;">Empowering Canadian professionals to build wealth.</span>
+    </div>
+""", unsafe_allow_html=True)
